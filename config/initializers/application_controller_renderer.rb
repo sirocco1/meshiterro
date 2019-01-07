fb762c233cba965c7a87e182353b5c96435b574b
sirocco1 <shino2163@gmail.com>
2019-01-07 21:08:42 +0900

    [Finish] 5-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '54c731c30346aa5d21133eedebbce8cd9bb14be20189e4d4ea3f5f098db9383f1e417aa37be199d4cbf59542ab45c52562b215625e2aefb29648339dd09c181e'
